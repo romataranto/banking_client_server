271f487dcbf9cf9a73bfbe69de6f5e890c4931de
Roma Taranto <rtaranto@smith.edu>
2023-11-08 22:29:38 -0500

    Prelim multi-connection

--- atm_client.py
+++ atm_client.py
@@ -3,9 +3,15 @@
 # Automated Teller Machine (ATM) client application.
 
 import socket
+# import sys
+# import selectors
+# import types
 
 HOST = "127.0.0.1"      # The bank server's IP address
 PORT = 65432            # The port used by the bank server
+
+# sel = selectors.DefaultSelector()
+# messages = [b"Message 1 from client.", b"Message 2 from client."]
 
 ##########################################################
 #                                                        #
@@ -14,6 +20,25 @@ PORT = 65432            # The port used by the bank server
 # NEEDS REVIEW. Changes may be needed in this section.   #
 #                                                        #
 ##########################################################
+
+# def start_connections(host, port, num_conns):
+#     server_addr = (host, port)
+#     for i in range(0, num_conns):
+#         connid = i + 1
+#         print(f"Starting connection {connid} to {server_addr}")
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         sock.setblocking(False)
+#         sock.connect_ex(server_addr)
+#         events = selectors.EVENT_READ | selectors.EVENT_WRITE
+#         data = types.SimpleNamespace(
+#             connid=connid,
+#             msg_total=sum(len(m) for m in messages),
+#             recv_total=0,
+#             messages=messages.copy(),
+#             outb=b"",
+#         )
+#         sel.register(sock, events, data=data)
+
 
 def send_to_server(sock, msg):
     """ Given an open socket connection (sock) and a string msg, send the string to the server. """
--- bank_server.py
+++ bank_server.py
@@ -4,11 +4,20 @@
 # Jimmy da Geek
 
 import socket
+# import selectors
+# import types
 
 HOST = "127.0.0.1"      # Standard loopback interface address (localhost)
 PORT = 65432            # Port to listen on (non-privileged ports are > 1023)
 ALL_ACCOUNTS = dict()   # initialize an empty dictionary
 ACCT_FILE = "accounts.txt"
+# sel = selectors.DefaultSelector()
+# lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# lsock.bind((HOST, PORT))
+# lsock.listen()
+# print(f"Listening on {(HOST, PORT)}")
+# lsock.setblocking(False)
+# sel.register(lsock, selectors.EVENT_READ, data=None)
 
 ##########################################################
 #                                                        #
@@ -155,8 +164,54 @@ def send_balance_to_client(conn, account):
 #                                                        #
 ##########################################################
 
+# def accept_wrapper(sock):
+#     conn, addr = sock.accept()  # Should be ready to read
+#     print(f"Accepted connection from {addr}")
+#     conn.setblocking(False)
+#     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+#     events = selectors.EVENT_READ | selectors.EVENT_WRITE
+#     sel.register(conn, events, data=data)
+
+# def service_connection(key, mask):
+#     sock = key.fileobj
+#     data = key.data
+#     if mask & selectors.EVENT_READ:
+#         recv_data = sock.recv(1024)  # Should be ready to read
+#         if recv_data:
+#             #data.outb += recv_data
+#             print(f"Received {recv_data!r} from connection {data.connid}")
+#             data.recv_total += len(recv_data)
+#         # else:
+#         #     print(f"Closing connection to {data.addr}")
+#         if not recv_data or data.recv_total == data.msg_total:
+#             print(f"Closing connection {data.connid}")
+#             sel.unregister(sock)
+#             sock.close()
+#     if mask & selectors.EVENT_WRITE:
+#         if not data.outb and data.messages:
+#             data.outb = data.messages.pop(0)
+#         if data.outb:
+#             #print(f"Echoing {data.outb!r} to {data.addr}")
+#             print(f"Sending {data.outb!r} to connection {data.connid}")
+#             sent = sock.send(data.outb)  # Should be ready to write
+#             data.outb = data.outb[sent:]
+
 def run_network_server():
     """ This and all supporting code needs to be written! """
+
+    # try:
+    #     while True:
+    #         events = sel.select(timeout=None)
+    #         for key, mask in events:
+    #             if key.data is None:
+    #                 accept_wrapper(key.fileobj)
+    #             else:
+    #                 service_connection(key, mask)
+    # except KeyboardInterrupt:
+    #     print("Caught keyboard interrupt, exiting")
+    # finally:
+    #     sel.close()
+
     x = 0 # init counter variable
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
